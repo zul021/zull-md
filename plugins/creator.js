22f4e0d2024ee2321d052aabab9cc898f443acf2
Mursid S <97183746+Botwa021@users.noreply.github.com>
2022-06-16 14:14:19 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,9 +5,9 @@ async function handler(m) {
                     + 'N:;Ilman;;;'
                     + 'FN:Ilman\n' // full name
                     + 'ORG:Ilman;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281351047727:+62 813-5104-7727\n' // WhatsApp ID + phone number
+                    + 'TEL;type=CELL;type=VOICE;waid=601127316764:+60 11-2731 6764\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Ilman', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Forgt-Zull', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
